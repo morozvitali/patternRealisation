76e4e70d7e1d42104fb8798114a8988d1215d882
morozvitali <morozvitali99@gmail.com>
2025-03-05 14:47:58 +0200

    minor correction

--- src/main/java/com/moroz/fabric/FactoryMethod.java
+++ src/main/java/com/moroz/fabric/FactoryMethod.java
@@ -3,13 +3,19 @@ package main.java.com.moroz.fabric;
 import main.java.com.moroz.fabric.creator.CreateCheese;
 import main.java.com.moroz.fabric.creator.CreateVine;
 import main.java.com.moroz.fabric.creator.Creator;
+import main.java.com.moroz.fabric.product.Cheese;
+import main.java.com.moroz.fabric.product.Vine;
 
 public class FactoryMethod {
 
     public static void main(String[] args) {
-        Creator vine = new CreateVine();
-        vine.someOperation();
-        Creator cheese = new CreateCheese();
-        cheese.someOperation();
+        Creator creatorvine = new CreateVine();
+        creatorvine.someOperation();
+        Vine vine = (Vine) creatorvine.createProduct();
+
+
+        Creator creatorcheese = new CreateCheese();
+        creatorcheese.someOperation();
+        Cheese cheese = (Cheese) creatorcheese.createProduct();
     }
 }
